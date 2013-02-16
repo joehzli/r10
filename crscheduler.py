eca9fc80650d6352164d8bf29a662fb7f41e8197
Joe <joelai85@gmail.com>
2013-02-16 02:53:44 -0500

    implement own queue and make it thread safe.

--- crscheduler.py
+++ crscheduler.py
@@ -1,14 +1,42 @@
 import Queue
 import hashlib
+import threading
 from singleton import singleton
+
+# thread safe Queue
+class CRQueue:
+	def __init__(self) :
+		self.mutex = threading.Lock()
+		self.queue = []
+		self.size = 0
+	def put(self, item) :
+		self.mutex.acquire()
+		self.queue.append(item)
+		self.size += 1
+		self.mutex.release()
+	def get(self) :
+		self.mutex.acquire()
+		if self.size == 0:
+			item = None
+		else:
+			item = self.queue[0]
+			del self.queue[0]
+			self.size -= 1
+		self.mutex.release()
+		return item
+	def qsize(self) :
+		self.mutex.acquire()
+		result = self.size
+		self.mutex.release()
+		return result
 
 @singleton
 class Scheduler:
 	_maxdownload = 0
 
 	def __init__(self):
-		self.queue = Queue.Queue()
-		self.downloaded = {}
+		self.queue = CRQueue()
+		self.downloaded = {} # Python's dict is already thread-safe
 
 	def size(self):
 		return self.queue.qsize()
@@ -24,6 +52,9 @@ class Scheduler:
 
 	def setMaxDownload(self, theMax):
 		self._maxdownload = theMax
+
+	def getMaxDownload(self):
+		return self._maxdownload
 
 	def downloadedAmount(self):
 		return len(self.downloaded)
