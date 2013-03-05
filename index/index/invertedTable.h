e89518a34300f1dad9cc5dbfbed32235970eece7
Joe <joelai85@gmail.com>
2013-03-04 22:55:57 -0500

    update invertedtable definition.

--- index/index/invertedTable.h
+++ index/index/invertedTable.h
@@ -10,5 +10,28 @@
 #define __index__invertedTable__
 
 #include <iostream>
+#include <string>
+#include <vector>
+
+typedef struct {
+    uint32_t pos;   // 12-bit is enough
+    uint16_t context;   //3-bit is enough
+} PostingTuple;
+
+typedef struct {
+    uint32_t docID;
+    std::string word;
+    uint32_t post;
+    uint16_t context;
+} RawPosting;
+
+typedef struct {
+    uint32_t docID; //27-bit is enough
+    uint32_t TF;
+    uint32_t length;    //5-bit??
+    PostingTuple *posArray;
+} DocTuple;
+
+typedef std::vector<RawPosting *> RawPostingVector;
 
 #endif /* defined(__index__invertedTable__) */
