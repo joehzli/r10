560ef524d446637f72eb2dd554188962833673d6
Joe <joelai85@gmail.com>
2013-03-04 22:54:48 -0500

    add utility.

---
+++ index/index/Utility.cpp
@@ -0,0 +1,9 @@
+//
+//  Utility.cpp
+//  index
+//
+//  Created by Hanzhou Li on 3/3/13.
+//  Copyright (c) 2013 Hanzhou Li. All rights reserved.
+//
+
+#include "Utility.h"
---
+++ index/index/Utility.h
@@ -0,0 +1,19 @@
+//
+//  Utility.h
+//  index
+//
+//  Created by Hanzhou Li on 3/3/13.
+//  Copyright (c) 2013 Hanzhou Li. All rights reserved.
+//
+
+#ifndef __index__Utility__
+#define __index__Utility__
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+typedef std::vector<char*> CStringVector;
+typedef std::vector<std::string> StringVector;
+
+#endif /* defined(__index__Utility__) */
